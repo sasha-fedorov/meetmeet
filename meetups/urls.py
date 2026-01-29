f6665b188632d16a3d8f891f6cb32e2c757fd4c5
sasha-fedorov <36fedorov@gmail.com>
2026-01-29 18:11:25 +0100

    refactor: rename meetup list URL to follow conventions

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.MeetupsListView.as_view(), name='home'),
+    path('', views.MeetupsListView.as_view(), name='meetup_list'),
     path('meetups/<int:pk>/', views.MeetupDetailView.as_view(), name="meetup_detail"),
 ]
