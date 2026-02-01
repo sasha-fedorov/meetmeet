e0f96e102551c6d3c528f49e2ff2110099427a94
sasha-fedorov <36fedorov@gmail.com>
2026-02-01 15:44:08 +0100

    chore: format settings to comply with PEP 8

--- meetmeet/settings.py
+++ meetmeet/settings.py
@@ -104,16 +104,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
     },
 ]
 
@@ -145,4 +149,5 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to
     # reduce disk use and renames the files with unique names for each version
     # to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.' \
+                          'CompressedManifestStaticFilesStorage'
