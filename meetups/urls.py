241d0a34435451c88f842a1bffe6bdd743013ae7
sasha-fedorov <36fedorov@gmail.com>
2026-02-01 19:52:11 +0100

    feat: add About page

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.MeetupsListView.as_view(), name='meetup_list'),
@@ -11,4 +12,6 @@ urlpatterns = [
          views.MeetupUpdateView.as_view(), name='meetup_update'),
     path('meetups/<int:pk>/delete/',
          views.MeetupDeleteView.as_view(), name='meetup_delete'),
+    path('about/', TemplateView.as_view(template_name='about.html'),
+         name='about'),
 ]
