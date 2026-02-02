ff2fe1043e62be9e3fc9ac3ea0ec85b938c62b7a
sasha-fedorov <36fedorov@gmail.com>
2026-02-02 01:02:50 +0100

    chore: add code comments to urls

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    # General List and Detail
     path('', views.MeetupsListView.as_view(), name='meetup_list'),
     path('meetups/<int:pk>/',
          views.MeetupDetailView.as_view(), name="meetup_detail"),
+
+    # Meetup Management (CRUD)
     path('meetups/create/',
          views.MeetupCreateView.as_view(), name='meetup_create'),
     path('meetups/<int:pk>/edit/',
@@ -13,6 +16,7 @@ urlpatterns = [
     path('meetups/<int:pk>/delete/',
          views.MeetupDeleteView.as_view(), name='meetup_delete'),
 
+    # Participation Management
     path('meetup/<int:pk>/toggle-participation/',
          views.ToggleParticipationView.as_view(), name='toggle_participation'),
     path('participation/<int:pk>/approve/',
@@ -20,6 +24,7 @@ urlpatterns = [
     path('participation/<int:pk>/reject/',
          views.reject_participation, name='reject_participation'),
 
+    # Static Pages
     path('about/', TemplateView.as_view(template_name='about.html'),
          name='about'),
 ]
